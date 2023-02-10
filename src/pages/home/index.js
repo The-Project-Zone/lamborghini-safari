764839ba3aa51679f6d05f9bd3328fe870f16ba7
sricharankrishnan <sricharan.krishnan@gmail.com>
2023-02-10 18:10:09 +0530

    [Build] 10022023_1810
    1. created the footer video section
    2. placed required sample creatives

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -8,21 +8,14 @@ import {HomeJumbotronBanner} from "./components/jumbotron-banner.js";
 import {HorizontalScroller} from "./components/horizontal-scroller.js";
 import {ReactHTML5BackgroundVideo} from "@components/react-html5-bg-video/index.js";
 
+/* video imports */
 import jumbotronVideoMp4Source from "@appVideos/jumbotron-video.mp4";
 import jumbotronVideoWebmSource from "@appVideos/jumbotron-video.webm";
+import footerVideoMp4Source from "@appVideos/footer-video.mp4";
+import footerVideoWebmSource from "@appVideos/footer-video.webm";
 
 export const HomePage = () => {
-  let [value, setValue] = useState(0);
-
-  useEffect(() => {
-    setTimeout(() => {
-      console.log("value set on timeout", value);
-      setValue((prevState) => prevState + 1);
-    }, 3000);
-  }, [value]);
-
   const videos = {mp4: jumbotronVideoMp4Source, webm: jumbotronVideoWebmSource}
-
   return (
     <React.Fragment>
       <div className="jumbotronSection positionRelative">
@@ -36,6 +29,13 @@ export const HomePage = () => {
         <ReactHTML5BackgroundVideo {...videos}/>
       </div>
       <HorizontalScroller/>
+      <div className="footerVideoSection positionRelative">
+        <ReactHTML5BackgroundVideo mp4={footerVideoMp4Source} webm={footerVideoWebmSource}/>
+        <div className="posContainer text-center">
+          <h3>We Are Closer To The Moon</h3>
+          <p>Stay Tuned. <br className="d-none d-sm-none"/>Watch This Space For More</p>
+        </div>
+      </div>
     </React.Fragment>
   );
 };
